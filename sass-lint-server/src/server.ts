2b15606c5b0a4bdeade78f2dbad1eeb9a6f509e7
Thierry Michel <thmichel@gmail.com>
2017-11-02 10:54:15 +0100

    :sparkles: add (ruleId) to the message. :pencil2: add punctuation (to follow ESLint message format)

--- sass-lint-server/src/server.ts
+++ sass-lint-server/src/server.ts
@@ -233,9 +233,12 @@ function makeDiagnostic(msg): Diagnostic {
 
     let message;
     if (msg.message) {
-        message = msg.message;
+        message = `${msg.message}.`;
+        if (msg.ruleId) {
+            message += ` (${msg.ruleId})`;
+        }
     } else {
-        message = "Unknown error";
+        message = "Unknown error.";
     }
 
     return {
